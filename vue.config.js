48c906de149c1c9bdbe68ce068e23b5e76bf2904
2061360308 <2061360308@qq.com>
2024-05-26 15:53:26 +0800

    fix:输出publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const publicPath = process.env.NODE_ENV === 'production'
+  ? (process.env.GITHUB_ACTIONS ? '/' + process.env.GITHUB_REPOSITORY.split('/')[1] + '/' : '/')
+  : '/';
+
+console.log('Public Path:', publicPath);
+
+module.exports = {
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? (process.env.GITHUB_ACTIONS ? '/' + process.env.GITHUB_REPOSITORY.split('/')[1] + '/' : '/')
-    : '/',
-  transpileDependencies: true
-})
+  publicPath: publicPath
+}
