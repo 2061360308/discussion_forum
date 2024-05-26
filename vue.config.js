2d5d1c585485565f5964a2785e30906765a7d478
2061360308 <2061360308@qq.com>
2024-05-26 15:33:31 +0800

    fix:action下自动适配仓库相对路径

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? (process.env.GITHUB_ACTIONS ? '/' + process.env.GITHUB_REPOSITORY.split('/')[1] + '/' : '/')
+    : '/',
   transpileDependencies: true
 })
